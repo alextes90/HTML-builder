055085015620d9173db6a25a20872a8c97e87947
Aleksandr <a.teslin90@gmail.com>
2022-11-02 20:50:38 +0300

    feat: add copy and actualization to 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,47 @@
+const fs = require("fs/promises");
+const path = require("path");
+const pathAbs = path.join(__dirname, "files");
+const pathDest = path.join(__dirname, "files-copy");
 
+async function readDir() {
+  try {
+    createDir();
+    const arrLength = await fs.readdir(pathDest);
+    if (arrLength.length > 0) {
+      deleteDir();
+    }
+    const files = await fs.readdir(pathAbs);
+    for (const file of files) {
+      const curPath = path.join(__dirname, "files", file);
+      const newPath = path.join(__dirname, "files-copy", file);
+      copyFile(curPath, newPath);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+async function copyFile(pathAbs, pathDest) {
+  try {
+    await fs.copyFile(pathAbs, pathDest);
+  } catch (err) {
+    console.error(err);
+  }
+}
+async function createDir() {
+  try {
+    await fs.mkdir(pathDest, { recursive: true });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function deleteDir() {
+  try {
+    await fs.rm(pathDest, { recursive: true });
+    readDir();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+readDir();
